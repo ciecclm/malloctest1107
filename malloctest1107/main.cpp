e7d56d76347a02dfa08901b0313865836aa0bbab
ciecclm <clm@imnu.edu.cn>
2015-11-07 09:08:36 +0800

    添加了电话号码

--- malloctest1107/main.cpp
+++ malloctest1107/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
-
+//#define N 100
+struct student
+{
+    char name[100];
+    int age;
+    char sex[3];
+    char PN[12];
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    student *a;
+    student temp;
+    int i,j;
+    int N;
+    scanf("%d",&N);
+    //a=(int *)malloc(N*4);
+    a=new student[N];
+    if(a==0)
+        exit(0);
+    for (i=0;i<N;i++)
+        scanf("%s %s %d %s",a[i].name,a[i].sex,&a[i].age,a[i].PN);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-i;j++)
+        {
+            if(a[j].age>a[j+1].age)
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for (i=0;i<N;i++)
+        printf("姓名：%s 性别：%s 年 龄：%d 电话：%s\n",a[i].name,a[i].sex,a[i].age,a[i].PN);
+    delete a;
     return 0;
 }
